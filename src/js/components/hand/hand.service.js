b3f72f74a5c25f16cb8fbbafbb1bd6ffb7c8cf3d
Wes Reid <bwreid@gmail.com>
2016-10-20 14:51:53 -0600

    1 API call instead of 2 for cards

--- src/js/components/hand/hand.service.js
+++ src/js/components/hand/hand.service.js
@@ -7,21 +7,16 @@
   HandService.$inject = ['$http']
 
   const baseUrl = 'https://deckofcardsapi.com/api/deck'
-  const newDeckPath = '/new/shuffle/?deck_count=1'
-  const getAllPath = (id) => `/${id}/draw/?count=52`
+  const newDeckPath = '/new/draw/?count=52'
 
   function HandService ($http) {
     this.deck = []
     this.draw = () => {
       // Get the deck and its id
       // Make another request to get all the cards back
-      $http.get(baseUrl + newDeckPath)
-        .then(({ data: { deck_id }}) => {
-          return $http.get(baseUrl + getAllPath(deck_id))
-        })
-        .then(({ data: { cards } }) => {
-          console.log(cards)
-        })
+      $http.get(baseUrl + newDeckPath).then(({ data: { cards }}) => {
+        console.log(cards);
+      })
     }
   }
 })()
